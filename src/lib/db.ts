73a87852474871d39bf55125b4fda6bb2d1e6b1d
alexandros <homocalidus@gmail.com>
2024-10-31 04:48:39 +0100

    db basic setup

---
+++ src/lib/db.ts
@@ -0,0 +1,17 @@
+import mysql, { PoolOptions } from 'mysql2';
+
+const access: PoolOptions = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    waitForConnections: true,
+    connectionLimit: process.env.DB_CONNECTION_LIMIT,
+    maxIdle: process.env.DB_MAX_IDLE, 
+    idleTimeout: process.env.DB_IDLE_TIMEOUT,
+};
+
+const pool = mysql.createPool(access);
+
+export { pool }
--- types/env.d.ts
+++ types/env.d.ts
@@ -1,9 +1,13 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         DB_HOST:string;
+        DB_PORT:number;
         DB_NAME:string;
         DB_USER:string;
         DB_PASS:string; 
+        DB_CONNECTION_LIMIT:number;
+        DB_MAX_IDLE:number;
+        DB_IDLE_TIMEOUT:number;
         
     }
 } 
