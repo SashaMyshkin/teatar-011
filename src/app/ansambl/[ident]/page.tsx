dd111f504e3bbc5162adfb30e08fdf33a0115117
alexandros <homocalidus@gmail.com>
2024-11-02 17:48:31 +0100

    dynamic routes

--- src/app/ansambl/[ident]/page.tsx
+++ src/app/ansambl/[ident]/page.tsx
@@ -1,3 +1,8 @@
-export default function(){
-    return(<></>)
-}
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ ident: string }>
+  }) {
+    const ident = (await params).ident
+    return <div>My Post: {ident}</div>
+  }
